9c3d331e8e4e76c69d1046590df351d17c5df3fc
Rudolf, Henrik (SE-A/32) <henrik.rudolf@volkswagen.de>
2024-10-21 14:45:15 +0200

    add dashboard

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,18 @@
+import { HiOutlineMoon } from "react-icons/hi";
+import { HiOutlineSun } from "react-icons/hi2";
+
+import ButtonIcon from "./ButtonIcon";
+
+import { useDarkMode } from "../context/DarkModeContext";
+
+function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineMoon /> : <HiOutlineSun />}
+    </ButtonIcon>
+  );
+}
+
+export default DarkModeToggle;
